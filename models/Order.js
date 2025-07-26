4780aaa8c46aa1ecb3d4fe970dc13fe2d0636034
sakshi923898 <satyavita81@gmail.com>
2025-07-26 22:31:53 +0530

    ownerAuthRoutes

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   customerName: { type: String, required: true },
-  contactNumber: { type: String, required: true },
   address: { type: String, required: true },
   status: { type: String, default: 'Pending' },
   cakeId: {
