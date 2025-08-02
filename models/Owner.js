a4c4e3b8270ff21b9d7807e419de42f7801f08c6
sakshi923898 <satyavita81@gmail.com>
2025-08-02 18:58:23 +0530

    Add missing Owner model

--- models/Owner.js
+++ models/Owner.js
@@ -1,17 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const ownerSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   hashedPassword: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// const Owner = mongoose.model('Owner', ownerSchema);
+
+// module.exports = Owner;
+
 const mongoose = require('mongoose');
 
 const ownerSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  hashedPassword: {
-    type: String,
-    required: true,
-  },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-const Owner = mongoose.model('Owner', ownerSchema);
-
-module.exports = Owner;
+module.exports = mongoose.model('Owner', ownerSchema);
