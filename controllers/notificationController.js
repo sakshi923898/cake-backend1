6dcf46f5c89780516b3fa9b9ff9520fe0f70bbd3
sakshi923898 <satyavita81@gmail.com>
2025-08-06 21:39:56 +0530

    Add missing Owner model

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -2,6 +2,14 @@
 
 const Notification = require('../models/Notification');
 
+const getNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find().sort({ createdAt: -1 });
+    res.status(200).json(notifications);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch notifications' });
+  }
+};
 const createNotification = async (req, res) => {
   try {
     const { message } = req.body;
