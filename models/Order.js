85d725a4a79dd45fa9ee160cd39352885e2b1ee7
sakshi923898 <satyavita81@gmail.com>
2025-08-07 15:39:24 +0530

    Add missing Owner model

--- models/Order.js
+++ models/Order.js
@@ -1,23 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const orderSchema = new mongoose.Schema({
-//   customerName: String,
-//   contact: {
-//   type: String,
-//   required: true,
-// },          
-//   address: String,
-//   cakeId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Cake'
-//   },
-//   status: {
-//     type: String,
-//     default: 'Pending'
-//   }
-// });
-
-// module.exports = mongoose.model('Order', orderSchema);
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
