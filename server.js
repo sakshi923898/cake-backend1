65cae48f858ab28758e672407c7b2ad61655abb2
sakshi923898 <satyavita81@gmail.com>
2025-09-02 13:12:47 +0530

    Add missing Owner model

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('./config/cloudinary');
 require('dotenv').config();
 
-const ownerAuthRoutes = require('./routes/ownerAuthRoutes');
+const ownerRoutes = require('./routes/ownerAuthRoutes');
 const Order = require('./models/Order');
 const app = express();
 const Notification = require('./models/Notification');
