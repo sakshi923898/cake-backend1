cb16a205e1e488fc6315cee6554a6cc6b7034c3c
sakshi923898 <satyavita81@gmail.com>
2025-10-06 18:30:10 +0530

    Add missing Owner model

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const corsOptions = {
 // app.use(cors(corsOptions));
 app.use(cors({
   origin: 'https://samrth.netlify.app', // ✅ your frontend Netlify domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 app.use(express.json());
