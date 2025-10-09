b7b15544de1cdad1f9ece24efba979ee7f342501
sakshi923898 <satyavita81@gmail.com>
2025-10-09 10:29:17 +0530

    Add missing Owner model

--- routes/ownerAuthRoutes.js
+++ routes/ownerAuthRoutes.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Order = require('../models/Order');
 const verifyOwner = require('../middleware/verifyOwner');
+import { sendOrderEmail } from '../emailService.js';
 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
